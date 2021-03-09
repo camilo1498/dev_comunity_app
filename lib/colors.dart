2e79ee4e5173b0f5091cf9ebdbea8bad0338480f
camilo1498 <davidcamilo96000@gmail.com>
2021-03-09 09:53:21 -0500

    second commit

---
+++ lib/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ConstantColors {
+  static final bgColorFaint = Color.fromRGBO(33, 37, 41, 1);
+  static final bgColor = Color.fromRGBO(10, 1, 22, 0.5);
+  static final darkColor = Colors.blueGrey.shade900;
+  static final whiteColor = Colors.white;
+  static final yellowColor = Color.fromRGBO(254, 228, 64, 1);
+}
